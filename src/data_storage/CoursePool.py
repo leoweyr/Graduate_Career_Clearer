c14bedfcc1ab27ad4c8f8acc964b9f18ff918c9d
leoweyr <2516427163@qq.com>
2024-02-15 20:02:43 +0800

    feat: add data pools for course and major

---
+++ src/data_storage/CoursePool.py
@@ -0,0 +1,24 @@
+from typing import List, Dict, Any
+
+from data_storage.Pool import Pool
+from data_model.Course import Course
+from data_model.DataModelable import DataModelable
+
+
+class CoursePool(Pool):
+    def __init__(self):
+        self.__courses: List[Course] = []
+
+    def add_data(self, data: DataModelable) -> None:
+        self.__courses.append(data)
+
+    def get_data(self, condition: Dict[str: Any] = None) -> List[DataModelable]:
+        search_results: Dict[int, DataModelable] = dict(self._find_data(self.__courses, condition))
+
+        return list(search_results.values())
+
+    def remove_data(self, condition: Dict[str: Any] = None) -> None:
+        search_results: Dict[int, DataModelable] = dict(self._find_data(self.__courses, condition))
+
+        for index in search_results.keys():
+            self.__courses.pop(index)
---
+++ src/data_storage/MajorPool.py
@@ -0,0 +1,24 @@
+from typing import List, Dict, Any
+
+from data_storage.Pool import Pool
+from data_model.Major import Major
+from data_model.DataModelable import DataModelable
+
+
+class MajorPool(Pool):
+    def __init__(self):
+        self.__majors: List[Major] = []
+
+    def add_data(self, data: DataModelable) -> None:
+        self.__majors.append(data)
+
+    def get_data(self, condition: Dict[str: Any] = None) -> List[DataModelable]:
+        search_results: Dict[int, DataModelable] = dict(self._find_data(self.__majors, condition))
+
+        return list(search_results.values())
+
+    def remove_data(self, condition: Dict[str: Any] = None) -> None:
+        search_results: Dict[int, DataModelable] = dict(self._find_data(self.__majors, condition))
+
+        for index in search_results.keys():
+            self.__majors.pop(index)
