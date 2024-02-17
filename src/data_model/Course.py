453825f3b4fc8d688f7465c74d2d53cfa57bb7e1
leoweyr <2516427163@qq.com>
2024-02-17 14:34:46 +0800

    feat: add Course.Builder for creating Course objects

--- src/data_model/Course.py
+++ src/data_model/Course.py
@@ -5,14 +5,15 @@ from data_storage.Storable import Storable
 from src.data_model.CourseNature import CourseNature
 from src.data_model.DataIncompleteError import DataIncompleteError
 from data_storage.DataNotIndexableError import DataNotIndexableError
+from pure_object_oriented.NoInheritMeta import NoInheritMeta
 
 
 class Course(DataModelable, Storable):
-    def __init__(self):
-        self._id: str = ""
-        self._name: str = ""
-        self._nature: CourseNature = CourseNature.UNKNOW
-        self._supplier: str = ""
+    def __init__(self, builder: 'Course.Builder'):
+        self._id: str = builder.outer_class_get_id()
+        self._name: str = builder.outer_class_get_name()
+        self._nature: CourseNature = builder.outer_class_get_nature()
+        self._supplier: str = builder.outer_class_get_supplier()
 
     def is_completed(self) -> bool:
         if self._id == "":
@@ -58,3 +59,57 @@ class Course(DataModelable, Storable):
             }
 
             return metadata
+
+    class Builder(metaclass=NoInheritMeta):
+        def __init__(self):
+            self.__id: str = ""
+            self.__name: str = ""
+            self.__nature: CourseNature = CourseNature.UNKNOW
+            self.__supplier: str = ""
+
+        def id(self, id: str) -> 'Course.Builder':
+            self.__id = id
+            return self
+
+        def name(self, name: str) -> 'Course.Builder':
+            self.__name = name
+            return self
+
+        def nature(self, nature: CourseNature) -> 'Course.Builder':
+            self.__nature = nature
+            return self
+
+        def supplier(self, supplier: str) -> 'Course.Builder':
+            self.__supplier = supplier
+            return self
+
+        def build(self) -> 'Course':
+            return Course(self)
+
+        def outer_class_get_id(self) -> str:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__id
+
+        def outer_class_get_name(self) -> str:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__name
+
+        def outer_class_get_nature(self) -> CourseNature:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__nature
+
+        def outer_class_get_supplier(self) -> str:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__supplier
