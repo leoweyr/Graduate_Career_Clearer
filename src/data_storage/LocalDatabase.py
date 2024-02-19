24c90ffe61c973f1450eeb14af27379ddbc4cf9d
leoweyr <2516427163@qq.com>
2024-02-19 12:48:58 +0800

    feat: add local serialization database

---
+++ src/data_storage/LocalDatabase.py
@@ -0,0 +1,70 @@
+import os.path
+from typing import List
+import pickle
+from datetime import datetime
+
+from data_storage.Databaseable import Databaseable
+from data_storage.DataContainer import DataContainer
+
+
+class LocalDatabase(Databaseable):
+    def __init__(self, path: str):
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"The path <{path}> does not exist.")
+        elif not os.path.isdir(path):
+            raise NotADirectoryError(f"The path <{path} is not a directory.")
+
+        self.__path: str = path
+
+        # Initialize or retrieve metadata tags if it already exists.
+        self.__metadata_tags: List[str] = []
+        metadata_tags_file_path: str = os.path.join(self.__path, ".metadata_tags")
+
+        if len(os.listdir(self.__path)) != 0 and os.path.isfile(metadata_tags_file_path):
+            with open(metadata_tags_file_path, "rb") as metadata_tags_file:
+                self.__metadata_tags = pickle.load(metadata_tags_file)
+
+    def pull(self) -> DataContainer:
+        if len(self.__metadata_tags) == 0:
+            return DataContainer()
+        else:
+            file_names: List[str] = os.listdir(self.__path)
+            time_serial_numbers: List[int] = [int(name.split('.')[0]) for name in file_names
+                                              if name.split('.')[0].isdigit()]
+            last_time_serial_number: int = max(time_serial_numbers)
+            data: DataContainer = None
+            data_file_name: str = str(last_time_serial_number) + ".db"
+            data_file_path: str = os.path.join(self.__path, data_file_name)
+            with open(data_file_path, "rb") as data_file:
+                data = pickle.load(data_file)
+
+        return data
+
+    def push(self, data: DataContainer) -> None:
+        static_data: DataContainer = data.clone()
+        static_data_metadata_tags: List[str] = static_data.get_metadata_tags()
+
+        if len(self.__metadata_tags) == 0:
+            self.__metadata_tags = static_data_metadata_tags
+            metadata_tags_file_path: str = os.path.join(self.__path, ".metadata_tags")
+            with open(metadata_tags_file_path, "wb") as metadata_tags_file:
+                pickle.dump(self.__metadata_tags, metadata_tags_file)
+        else:
+            if static_data_metadata_tags != self.__metadata_tags:
+                raise ValueError("The metadata tags of data container are not match the database requirement.")
+
+        # Serialize data and store to database.
+        data_file_name: str = str(datetime.now().strftime("%Y%m%d%H%M%S%f")) + ".db"
+        data_file_path: str = os.path.join(self.__path, data_file_name)
+        with open(data_file_path, "wb") as data_file:
+            pickle.dump(static_data, data_file)
+
+        # Clear historical data.
+        file_names: List[str] = os.listdir(self.__path)
+        time_serial_numbers: List[int] = [int(name.split('.')[0]) for name in file_names
+                                          if name.split('.')[0].isdigit()]
+        last_time_serial_number: int = max(time_serial_numbers)
+        file_to_keep: List[str] = [str(last_time_serial_number) + ".db", ".metadata_tags"]
+        for file_name in file_names:
+            if file_name not in file_to_keep:
+                os.remove(os.path.join(self.__path, file_name))
