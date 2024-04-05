0a1cc5082e059951a4d4e607b095c8ba47917a28
leoweyr <2516427163@qq.com>
2024-04-05 16:29:29 +0800

    refactor: DataIncompleteError and DataNotIndexableError

--- src/data_model/DataIncompleteError.py
+++ src/data_model/DataIncompleteError.py
@@ -1,6 +1,8 @@
-from typing import Any
+from data_model.DataModelable import DataModelable
 
 
 class DataIncompleteError(Exception):
-    def __init__(self, object_reference: Any, missing_data: str):
-        super().__init__(str(object_reference) + " is missing data <" + missing_data + ">")
+    def __init__(self, data_modelable_object: DataModelable, missing_data: str):
+        self.__data_modelable_object: DataModelable = data_modelable_object
+        self.__missing_data: str = missing_data
+        super().__init__(f"{repr(self.__data_modelable_object)} is missing data <{self.__missing_data}>.")
--- src/data_storage/DataNotIndexableError.py
+++ src/data_storage/DataNotIndexableError.py
@@ -1,6 +1,8 @@
-from typing import Any
+from data_storage.Storable import Storable
 
 
 class DataNotIndexableError(Exception):
-    def __init__(self, object_reference: Any, missing_metadata: str):
-        super().__init__(str(object_reference) + " is missing metadata <" + missing_metadata + ">")
+    def __init__(self, storable_object: Storable, missing_metadata: str):
+        self.__storable_object: Storable = storable_object
+        self.__missing_metadata: str = missing_metadata
+        super().__init__(f"{repr(self.__storable_object)} is missing metadata <{self.__missing_metadata}>.")
