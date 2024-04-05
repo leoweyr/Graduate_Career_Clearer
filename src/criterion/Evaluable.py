f50eeb162e0c73f7777ba14f4086533e97db2845
leoweyr <2516427163@qq.com>
2024-04-05 10:38:23 +0800

    refactor: change target parameter type to DataModelable in _judge and evaluate methods of criterion classes

--- src/criterion/Evaluable.py
+++ src/criterion/Evaluable.py
@@ -1,17 +1,19 @@
 from abc import ABCMeta, abstractmethod
-from typing import Any, Dict
+from typing import Dict, Any
+
+from data_model.DataModelable import DataModelable
 
 
 class Evaluable(metaclass=ABCMeta):
     @abstractmethod
-    def _judge(self, target: Any) -> Dict[str, Any]:
+    def _judge(self, target: DataModelable) -> Dict[str, Any]:
         pass
 
     @abstractmethod
     def _conclude(self, result: Dict[str, Any]) -> None:
         pass
 
-    def evaluate(self, target: Any) -> None:
+    def evaluate(self, target: DataModelable) -> None:
         """
         Non-overridable.
         """
--- src/criterion/PointCriterion.py
+++ src/criterion/PointCriterion.py
@@ -1,29 +1,33 @@
 from typing import Dict, Any
 
 from criterion import Evaluable
+from data_model.DataModelable import DataModelable
 from data_model.TakenCourse import TakenCourse
 from data_model.TakenCourseStatus import TakenCourseStatus
 
 
 class PointCriterion(Evaluable):
-    def _judge(self, target: TakenCourse) -> Dict[str, Any]:
+    def _judge(self, target: DataModelable) -> Dict[str, Any]:
         """
         According to the principle of strict control, priority is given to judging the passing situation to avoid
         misjudgment as failing due to errors in the original data format.
         """
-        if isinstance(target.points, float):
-            if float(target.points) >= 60:
-                return dict({"target": target, "is_pass": True})
-        elif isinstance(target.points, str):
-            if str(target.points) == "及格":
-                return dict({"target": target, "is_pass": True})
-            elif str(target.points) == "良好":
-                return dict({"target": target, "is_pass": True})
-            elif str(target.points) == "优秀":
-                return dict({"target": target, "is_pass": True})
-            elif str(target.points) == "中等":
-                return dict({"target": target, "is_pass": True})
-        return dict({"target": target, "is_pass": False})
+        if target.is_completed():
+            target_points: Any = target.get_data()["points"]
+
+            if isinstance(target_points, float):
+                if float(target_points) >= 60:
+                    return dict({"target": target, "is_pass": True})
+            elif isinstance(target_points, str):
+                if str(target_points) == "及格":
+                    return dict({"target": target, "is_pass": True})
+                elif str(target_points) == "良好":
+                    return dict({"target": target, "is_pass": True})
+                elif str(target_points) == "优秀":
+                    return dict({"target": target, "is_pass": True})
+                elif str(target_points) == "中等":
+                    return dict({"target": target, "is_pass": True})
+            return dict({"target": target, "is_pass": False})
 
     def _conclude(self, result: Dict[str, Any]) -> None:
         evaluated_target: TakenCourse = result["target"]
