8e8c0f57b4c5f9bdc9f7f1e98e9fb9ce58b448c5
leoweyr <2516427163@qq.com>
2024-04-07 22:01:07 +0800

    feat: add builders for Graduate and Major

--- src/data_model/Graduate.py
+++ src/data_model/Graduate.py
@@ -1,16 +1,18 @@
-from typing import Union, List, Dict, Any
+from typing import Union, Any, List, Dict
 
 from data_model.DataModelable import DataModelable
 from data_storage.Storable import Storable
 from data_model.Gender import Gender
 from data_model.Major import Major
 from data_model.TakenCourse import TakenCourse
+from data_model.TypeCastingError import TypeCastingError
 from data_model.DataIncompleteError import DataIncompleteError
 from data_storage.DataNotIndexableError import DataNotIndexableError
+from pure_object_oriented.NoInheritMeta import NoInheritMeta
 
 
 class Graduate(DataModelable, Storable):
-    def __init__(self):
+    def __init__(self, builder: Union['Graduate.Builder', Any]):
         # Basic student information.
         self.__id: int = 0
         self.__name: str = ""
@@ -26,7 +28,48 @@ class Graduate(DataModelable, Storable):
         self.__major_optional_credits: float = 0
         self.__limited_elective_credits: float = 0
         self.__optional_credits: float = 0
-        self.__courses: List[TakenCourse] = []
+        self.__taken_courses: List[TakenCourse] = []
+
+        if isinstance(builder, Graduate.Builder):
+            self.__id = builder.outer_class_get_id()
+            self.__name = builder.outer_class_get_name()
+            self.__gender = builder.outer_class_get_gender()
+            self.__grade = builder.outer_class_get_grade()
+            self.__class = builder.outer_class_get_class()
+            self.__college = builder.outer_class_get_college()
+            self.__major = builder.outer_class_get_major()
+        else:
+            converted_object_members: List[str] = dir(builder)
+            self_object_members: List[str] = ["_Graduate__id",
+                                              "_Graduate__name",
+                                              "_Graduate__gender",
+                                              "_Graduate__grade",
+                                              "_Graduate__class",
+                                              "_Graduate__college",
+                                              "_Graduate__major",
+                                              "_Graduate__gpa",
+                                              "_Graduate__required_credits",
+                                              "_Graduate__major_optional_credits",
+                                              "_Graduate__limited_elective_credits",
+                                              "_Graduate__optional_credits",
+                                              "_Graduate__taken_courses"]
+
+            if not set(self_object_members).issubset(set(converted_object_members)):
+                raise TypeCastingError(builder, Graduate)
+            else:
+                self.__id = getattr(builder, "_Graduate__id")
+                self.__name = getattr(builder, "_Graduate__name")
+                self.__gender = getattr(builder, "_Graduate__gender")
+                self.__grade = getattr(builder, "_Graduate__grade")
+                self.__class = getattr(builder, "_Graduate__class")
+                self.__college = getattr(builder, "_Graduate__college")
+                self.__major = getattr(builder, "_Graduate__major")
+                self.__gpa = getattr(builder, "_Graduate__gpa")
+                self.__required_credits = getattr(builder, "_Graduate__required_credits")
+                self.__major_optional_credits = getattr(builder, "_Graduate__major_optional_credits")
+                self.__limited_elective_credits = getattr(builder, "_Graduate__limited_elective_credits")
+                self.__optional_credits = getattr(builder, "_Graduate__optional_credits")
+                self.__taken_courses = getattr(builder, "_Graduate__taken_courses")
 
     def is_completed(self) -> bool:
         if self.__id == 0:
@@ -53,8 +96,8 @@ class Graduate(DataModelable, Storable):
             raise DataIncompleteError(self, "limited_elective_credits")
         elif self.__optional_credits == 0:
             raise DataIncompleteError(self, "optional_credits")
-        elif len(self.__courses) == 0:
-            raise DataIncompleteError(self, "courses")
+        elif len(self.__taken_courses) == 0:
+            raise DataIncompleteError(self, "taken_courses")
         else:
             return True
 
@@ -71,7 +114,7 @@ class Graduate(DataModelable, Storable):
             "major_optional_credits": self.__major_optional_credits,
             "limited_elective_credits": self.__limited_elective_credits,
             "optional_credits": self.__optional_credits,
-            "courses": self.__courses
+            "taken_courses": self.__taken_courses
         }
 
         return data_structure
@@ -107,3 +150,86 @@ class Graduate(DataModelable, Storable):
             }
 
             return metadata
+
+    class Builder(metaclass=NoInheritMeta):
+        def __init__(self):
+            self.__id: int = 0
+            self.__name: str = ""
+            self.__gender: Gender = Gender.UNKNOWN
+            self.__grade: int = 0
+            self.__class: str = ""
+            self.__college: str = ""
+            self.__major: Union[Major, None] = None
+
+        def id_(self, id_: int) -> None:
+            self.__id = id_
+
+        def name(self, name: str) -> None:
+            self.__name = name
+
+        def gender(self, gender: Gender) -> None:
+            self.__gender = gender
+
+        def grade(self, grade: int) -> None:
+            self.__grade = grade
+
+        def class_(self, class_: str) -> None:
+            self.__class = class_
+
+        def college(self, college: str) -> None:
+            self.__college = college
+
+        def major(self, major: Major) -> None:
+            self.__major = major
+
+        def build(self) -> 'Graduate':
+            return Graduate(self)
+
+        def outer_class_get_id(self) -> int:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__id
+
+        def outer_class_get_name(self) -> str:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__name
+
+        def outer_class_get_gender(self) -> Gender:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__gender
+
+        def outer_class_get_grade(self) -> int:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__grade
+
+        def outer_class_get_class(self) -> str:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__class
+
+        def outer_class_get_college(self) -> str:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__college
+
+        def outer_class_get_major(self) -> Union[Major, None]:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__major
--- src/data_model/Major.py
+++ src/data_model/Major.py
@@ -1,14 +1,16 @@
-from typing import List, Dict, Any
+from typing import Union, Any, List, Dict
 
 from data_model.DataModelable import DataModelable
 from data_storage.Storable import Storable
 from data_model.Course import Course
+from data_model.TypeCastingError import TypeCastingError
 from data_model.DataIncompleteError import DataIncompleteError
 from data_storage.DataNotIndexableError import DataNotIndexableError
+from pure_object_oriented.NoInheritMeta import NoInheritMeta
 
 
 class Major(DataModelable, Storable):
-    def __init__(self):
+    def __init__(self, builder: Union['Major.Builder', Any]):
         # Metadata.
         self.__id: int = 0
         self.__name: str = ""
@@ -19,6 +21,30 @@ class Major(DataModelable, Storable):
         self.__standard_limited_elective_credits: float = 0
         self.__standard_optional_credits: float = 0
         self.__standard_required_courses: List[Course] = []
+
+        if isinstance(builder, Major.Builder):
+            self.__id = builder.outer_class_get_id()
+            self.__name = builder.outer_class_get_name()
+            self.__edition = builder.outer_class_get_edition()
+        else:
+            converted_object_members: List[str] = dir(builder)
+            self_object_members: List[str] = ["_Major__id",
+                                              "_Major__name",
+                                              "_Major__edition",
+                                              "_Major__standard_major_optional_credits",
+                                              "_Major__standard_limited_elective_credits",
+                                              "_Major__standard_optional_credits",
+                                              "_Major__standard_required_courses"]
+
+            if not set(self_object_members).issubset(set(converted_object_members)):
+                raise TypeCastingError(builder, Major)
+            else:
+                self.__id = getattr(builder, "_Major__id")
+                self.__name = getattr(builder, "_Major__name")
+                self.__edition = getattr(builder, "_Major__edition")
+                self.__standard_major_optional_credits = getattr(builder, "_Major__standard_major_optional_credits")
+                self.__standard_limited_elective_credits = getattr(builder, "_Major__standard_limited_elective_credits")
+                self.__standard_required_courses = getattr(builder, "_Major__standard_required_courses")
 
     def __str__(self) -> str:
         return self.__name
@@ -73,3 +99,45 @@ class Major(DataModelable, Storable):
             }
 
             return metadata
+
+    class Builder(metaclass=NoInheritMeta):
+        def __init__(self):
+            self.__id: int = 0
+            self.__name: str = ""
+            self.__edition: int = 0
+
+        def id_(self, id_: int) -> 'Major.Builder':
+            self.__id = id_
+            return self
+
+        def name(self, name: str) -> 'Major.Builder':
+            self.__name = name
+            return self
+
+        def edition(self, edition: int) -> 'Major.Builder':
+            self.__edition = edition
+            return self
+
+        def build(self) -> 'Major':
+            return Major(self)
+
+        def outer_class_get_id(self) -> int:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__id
+
+        def outer_class_get_name(self) -> str:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__name
+
+        def outer_class_get_edition(self) -> int:
+            """
+            Due to Python's lack of support for outer class accessing private members of inner class, this method is
+            specifically designed to address this issue. Furthermore, it cannot be invoked externally.
+            """
+            return self.__edition
